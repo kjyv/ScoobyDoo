cbc2af4604e9c110a3df7ed9bf454e088e1604ae
rene <noodleboy@web.de>
2010-11-28 17:42:00 +0100

    fertig

--- src/Tokenize.java
+++ src/Tokenize.java
@@ -17,6 +17,7 @@ import com.aliasi.dict.MapDictionary;
 import com.aliasi.dict.ExactDictionaryChunker;
 import com.aliasi.tokenizer.IndoEuropeanTokenizerFactory;
 import com.aliasi.tokenizer.LowerCaseTokenizerFactory;
+import com.aliasi.tokenizer.RegExTokenizerFactory;
 import com.aliasi.tokenizer.Tokenizer;
 import com.aliasi.tokenizer.TokenizerFactory;
 import com.aliasi.tokenizer.StopTokenizerFactory;
@@ -38,39 +39,21 @@ import org.xml.sax.InputSource;
 public class Tokenize {
 
 	static final double CHUNK_SCORE = 1.0;
+	static StringBuilder result;
 	
 	/**
 	 * @param args
 	 * @throws IOException 
 	 */
 	public static void main(String[] args) throws IOException {
-		
-		/* possibility one: manually, maybe like this
-		 * (read in xml without parser) */
-		
-		MapDictionary<String> dictionary = new MapDictionary<String>();
-		
-		//for each gene name do this
-	    dictionary.addEntry(new DictionaryEntry<String>("50 Cent","PERSON",CHUNK_SCORE));
-		
-	    //create chunker (stop words?)
-		ExactDictionaryChunker dictionaryChunkerTF
-        = new ExactDictionaryChunker(dictionary,
-                                     IndoEuropeanTokenizerFactory.INSTANCE,true,false);
-		
-		//then chunk our medline articles (per article?)
-		String text = "50 cent is not in Medline";
-        chunk(dictionaryChunkerTF,text);
-        
-        
-        /* possibility two: use lingmed */
-        
+		long startTime = System.currentTimeMillis();
         boolean saveXML = false;
         MedlineParser parser = new MedlineParser(saveXML);
-        CitationHandler handler = new CitationHandler();
+        result = new StringBuilder("<results>\n");
+        CitationHandler handler = new CitationHandler(result);
         parser.setHandler(handler);
         for (String arg : args) {
-            System.out.println("Processing file=" + arg);
+            //System.out.println("Processing file=" + arg);
             if (arg.endsWith(".xml")) {
                 InputSource inputSource = new InputSource(arg);
                 parser.parse(inputSource);
@@ -78,58 +61,73 @@ public class Tokenize {
         		throw new IllegalArgumentException("arguments must end with .xml");
         	}
         }
-        handler.report();
+        result.append("</results>");
+        System.out.println(result);
+        System.out.println("time: " + (System.currentTimeMillis() - startTime)/1000.0);
 	}
-	
-    static void chunk(ExactDictionaryChunker chunker, String text) {
-        System.out.println("\nChunker."
-                           + " All matches=" + chunker.returnAllMatches()
-                           + " Case sensitive=" + chunker.caseSensitive());
-        Chunking chunking = chunker.chunk(text);
-        for (Chunk chunk : chunking.chunkSet()) {
-            int start = chunk.start();
-            int end = chunk.end();
-            String type = chunk.type();
-            double score = chunk.score();
-            String phrase = text.substring(start,end);
-            System.out.println("     phrase=|" + phrase + "|"
-                               + " start=" + start
-                               + " end=" + end
-                               + " type=" + type
-                               + " score=" + score);
-        }
-    }
 
     static class CitationHandler implements MedlineHandler {
         long mCitationCount = 0L;
         ObjectToCounterMap<String> mCounter = new ObjectToCounterMap<String>();
         
-        Set<String> stopSet = new HashSet();
+        Set<String> stopSet = new HashSet<String>(),
+        			geneSet = new HashSet<String>();
         String pmid;
+        StringBuilder result;
+        LowerCaseTokenizerFactory lcFactory;
         
-        public CitationHandler() throws IOException{
+        public CitationHandler(StringBuilder result) throws IOException{
         	 BufferedReader stopWords = new BufferedReader(new FileReader("english_stop_words.txt"));
         	 String line;
         	 while ((line = stopWords.readLine()) != null){
+        		 if(line.equals(""))
+        			 continue;
         		 stopSet.add(line);
         	 }
+        	 stopWords.close();
+        	 
+        	 BufferedReader geneNames = new BufferedReader(new FileReader("human-genenames.txt"));
+        	 //String line;
+        	 while ((line = geneNames.readLine()) != null){
+        		 if(line.length() == 0)
+        			 continue;
+        		 geneSet.add(line);
+        	 }
+        	 geneNames.close();
+        	 
+        	//set up tokenizers
+             //TokenizerFactory factory = IndoEuropeanTokenizerFactory.INSTANCE;
+        	 RegExTokenizerFactory factory = new RegExTokenizerFactory("\\S+");
+             StopTokenizerFactory stopFactory = new StopTokenizerFactory(factory, stopSet);
+             lcFactory = new LowerCaseTokenizerFactory(stopFactory);
+        	 
+        	 this.result = result;
         }
         
         public void handle(MedlineCitation citation) {
             ++mCitationCount;
+            result.append("<MedlineCitation>\n");
 
             pmid = citation.pmid();
+            result.append("<PMID>"+pmid+"</PMID>\n");
             // System.out.println("processing pmid=" + id);
 
             Article article = citation.article();
             String titleText = article.articleTitleText();
+            result.append("<ArticleTitle>\n");
             addText(titleText);
+            result.append("\n</ArticleTitle>\n");
 
             Abstract abstrct = article.abstrct();
-            if (abstrct != null) {
-                String abstractText = abstrct.textWithoutTruncationMarker();
+            String abstractText;
+            if (abstrct != null && !(abstractText = abstrct.textWithoutTruncationMarker()).equals(""))
+            {
+                result.append("<AbstractText>\n");
                 addText(abstractText);
+                result.append("\n</AbstractText>\n");
             }
+            
+            result.append("</MedlineCitation>\n");
 
             /*MeshHeading[] headings = citation.meshHeadings();
             for (MeshHeading heading : headings) {
@@ -143,28 +141,24 @@ public class Tokenize {
             throw new UnsupportedOperationException("not expecting deletes. found pmid=" + pmid);
         }
         public void addText(String text) {
+        	StringBuilder localResult = new StringBuilder(text);
+        	int numFoundGenes = 0;
             char[] cs = text.toCharArray();
-            
-            //set up tokenizers
-            TokenizerFactory factory = IndoEuropeanTokenizerFactory.INSTANCE;
-            StopTokenizerFactory stopFactory = new StopTokenizerFactory(factory, stopSet);
-            LowerCaseTokenizerFactory lcFactory = new LowerCaseTokenizerFactory(stopFactory);
             
             Tokenizer tokenizer = lcFactory.tokenizer(cs,0,cs.length);
             for (String token : tokenizer) {
+            	
                 mCounter.increment(token);
-                //TODO: go through all our genes here and search for token.. slooow
+                if(geneSet.contains(token))
+                {
+                	//System.out.println(token);
+                	//System.out.println("start: " + tokenizer.lastTokenStartPosition());
+                	localResult.insert(tokenizer.lastTokenStartPosition()+numFoundGenes*13, "<gene>");
+                	localResult.insert(tokenizer.lastTokenEndPosition()+numFoundGenes*13+6, "</gene>");
+                	numFoundGenes++;
+                }
             }
-        }
-        public void report() {
-            System.out.println("\nCitation Count=" + mCitationCount);
-            System.out.println("\nWord Counts");
-            List<String> keysByCount = mCounter.keysOrderedByCountList();
-            for (String key : keysByCount) {
-                int count = mCounter.getCount(key);
-                if (count < 10) break;
-                System.out.printf("%9d %s\n",count,key);
-            }
+            result.append(localResult);
         }
     }
 }
